dbd3e97d4771897b5e0e0780a9746b4ab0cef3ef
Jeremy Johnston <jpj054000@gmail.com>
2023-02-09 01:42:38 -0600

    flask time

--- server.py
+++ server.py
@@ -1,23 +1,20 @@
-from wsgiref.simple_server import make_server
-from pyramid.config import Configurator
-from pyramid.response import Response
+from flask import Flask, render_template
+
+server = Flask(__name__)
+
 import os
 
-def hello_world(request):
-    # name = os.environ.get('NAME')
-    # if name == None or len(name) == 0:
-    #     name = "world"
-    # message = "Hello, " + name + "!\n"
-    index_file = open("index.html", "r")
-    message = index_file.read()
-    index_file.close()
-    return Response(message)
+@app.route('/')
+def homepage():
+
+    title = "Hello :)"
+    paragraph = ["placeholder"]
+
+    try:
+        return render_template("index.html", title = title, paragraph=paragraph)
+    except Exception, e:
+        return str(e)
 
 if __name__ == '__main__':
     port = int(os.environ.get("PORT"))
-    with Configurator() as config:
-        config.add_route('hello', '/')
-        config.add_view(hello_world, route_name='hello')
-        app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', port, app)
-    server.serve_forever()
+    server.run(debug = True, host='0.0.0.0', port, passthrough_errors=True)
